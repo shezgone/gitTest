cf03624f36ee5c9448e6c3b4049b64a4fd159b2a
user <dmsfkd00>
2022-01-15 15:46:46 +0900

    dd

--- main.py
+++ main.py
@@ -14,6 +14,6 @@ if __name__ == '__main__':
     print_hi('PyCharm')
     print_hi('Yongs')
     print_hi('JaYoung')
-    print_hi('JiSoo')
+    print_hi('hot')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
